57b5a904bbfc436486d16edfafcf199ecdfe840e
rstar2 <rstar2@abv.bg>
2018-03-30 14:03:10 +0300

    use bootstrap

--- models/PostCategory.js
+++ models/PostCategory.js
@@ -1,18 +1,30 @@
 const keystone = require('keystone');
+const utils = keystone.utils;
+const Types = keystone.Field.Types;
 
 /**
  * PostCategory Model
  * ==================
  */
 
-const PostCategory = new keystone.List('PostCategory', {
-	autokey: { path: 'slug', from: 'name', unique: true },
-});
+const PostCategory = new keystone.List('PostCategory');
 
 PostCategory.add({
 	name: { type: String, required: true },
+	slug: { type: String },
+	parent: { type: Types.Relationship, ref: 'PostCategory' },
+});
+
+PostCategory.schema.pre('save', function (next) {
+	// if 'slug' is still not entered use the obligatory 'name' field
+	// thus 'slug' is also obligatory
+	const slug = this.slug || this.name;
+	this.slug = utils.slug(slug);
+
+	next();
 });
 
 PostCategory.relationship({ ref: 'Post', path: 'posts', refPath: 'categories' });
 
+PostCategory.defaultColumns = 'name, slug';
 PostCategory.register();
